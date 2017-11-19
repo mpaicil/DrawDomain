c9c90934e0453585ad3d29f67b101e296ce177a6
necrix <mauricio.paicil@zweicom.com>
2017-11-19 19:31:21 -0300

    first commit

---
+++ src/main/java/com/slackmag/domain/almanaque/Draw.java
@@ -0,0 +1,43 @@
+package com.slackmag.domain.almanaque;
+
+import java.time.LocalDate;
+import com.slackmag.domain.almanaque.drawtype.*;
+import java.util.List;
+
+public class Draw {
+
+    private Integer drawId;
+    private LocalDate date;
+
+    private Loto loto;
+    private Revancha revancha;
+    private Desquite desquite;
+    private AhoraSi ahoraSi;
+    private List<Jubilazo> jubilazoList;
+    private Multiplicador multiplicador;
+
+
+    public Draw(Integer drawId, LocalDate date, Loto loto, Revancha revancha, Desquite desquite, AhoraSi ahoraSi, List<Jubilazo> jubilazos, Multiplicador multiplicador) {
+        this.drawId = drawId;
+        this.date = date;
+        this.loto = loto;
+        this.revancha = revancha;
+        this.desquite = desquite;
+        this.ahoraSi = ahoraSi;
+        this.jubilazoList = jubilazos;
+        this.multiplicador = multiplicador;
+    }
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Integer getDrawId() {
+        return drawId;
+    }
+
+    public Loto getLoto() {
+        return loto;
+    }
+}
